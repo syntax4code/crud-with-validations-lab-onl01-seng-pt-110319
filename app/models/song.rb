fdf7dbb0dc87c9da8d55052c8c9e5743d1911a72
Warren Harper II <warrenharper2@att.net>
2020-04-06 18:34:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,21 +1,21 @@
 class Song < ApplicationRecord
-    validates :title, presence: true
-    validates :artist_name, presence: true
-    validate :no_repeated_title
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validate :no_repeated_title
+  validates :release_year, presence: true, 
+      if: :released?
+  validate :date_not_possible
 
-    validates :release_year, presence: true,
-        if: :released?
-    validate :date_not_possible
-  end
   def no_repeated_title
-    if Song.any? {|s| s.title == title && s.artist_name == artist_name && s.release_year == release_year}
-        errors.add(:title, "can't add the same song twice")
-    end
+      if Song.any? {|s| s.title == title && s.artist_name == artist_name && s.release_year == release_year}
+          errors.add(:title, "can't add the same song twice")
+      end
   end
 
   def date_not_possible
-    if release_year.present? && release_year > Date.today.year
-        errors.add(:release_year, "release year can't be in the future")
-    end
+      if release_year.present? && release_year > Date.today.year
+          errors.add(:release_year, "release year can't be in the future")
+      end
+  end
 
 end
